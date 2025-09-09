ef093f4b67565d9f7e1b98dbe51294f49c07a107
danielluccio <daniel130877@gmail.com>
2025-09-09 12:22:52 -0300

    Resolvendo conflitos

--- src/App.java
+++ src/App.java
@@ -1,12 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        StringWrapper stringWrapper = new StringWrapper("Olá Mundo !!");
-        System.out.println(stringWrapper.getElement());
-
-        Generics generics = new Generics("Olá Mundo !!");
-        System.out.println(generics.getElement());
-
-        Generics generics2 = new Generics(2);
-        System.out.println(generics2.getElement());
+       
     }
 }
